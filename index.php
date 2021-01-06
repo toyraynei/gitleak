1470696ef56a03e3462f3f41e1722b7985fa4a71
toyraynei <toyraynei@gmail.com>
2021-01-06 23:26:43 +0800

    update

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+  $flag = "CTF{th1s_1s_gi3_13ak}";
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Version v5</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <script src="script.js"></script>
+    </body>
+</html>
